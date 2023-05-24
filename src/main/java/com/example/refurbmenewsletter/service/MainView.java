512bf3932ef8a31f3a180b0207bc1977989d7f99
GroellerKarim <karim.jonas.groeller@gmail.com>
2023-05-24 23:10:19 +0200

    Added Button, changed layout, finished for tomorrow

--- src/main/java/com/example/refurbmenewsletter/service/MainView.java
+++ src/main/java/com/example/refurbmenewsletter/service/MainView.java
@@ -13,6 +13,8 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.StreamResource;
 import lombok.val;
 
+import java.util.function.Predicate;
+
 
 @Route("/")
 public class MainView extends VerticalLayout{
@@ -23,37 +25,35 @@ public class MainView extends VerticalLayout{
     private TextField emailField;
     private Checkbox acceptedPrivacyPolicyBox;
 
+    private Button acceptButton;
 
     public MainView(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
         setWidthFull();
         setHeightFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
         setAlignItems(Alignment.CENTER);
 
         getStyle().set("background-image", "url(\"https://cdn.discordapp.com/attachments/621043978510270474/1110941961663299664/SaschaEleven_a_lush_green_forest_a4c2cb72-d17d-4a1a-9241-6cc5113b20e8.png");
 
         firstNameField = new TextField("Vorname");
-        firstNameField.setMinWidth(40, Unit.PERCENTAGE);
-        firstNameField.setMaxWidth(40, Unit.PERCENTAGE);
+        firstNameField.setWidth("45%");
         lastNameField = new TextField("Nachname");
-        lastNameField.setMinWidth(40, Unit.PERCENTAGE);
-        lastNameField.setMaxWidth(40, Unit.PERCENTAGE);
+        lastNameField.setWidth("45%");
 
         StreamResource imageResource = new StreamResource("logo",
                 () -> getClass().getResourceAsStream("/images/logo.png"));
 
         Image image = new Image(imageResource, "RefurbMe Logo");
-        image.getStyle().set("padding-top", "30%");
+        image.getStyle().set("padding-top", "10%");
         image.setWidthFull();
         HorizontalLayout imageLayout = new HorizontalLayout(image);
-
 
         imageLayout.setWidthFull();
         imageLayout.setHeight("300px");
 
-        HorizontalLayout div = new HorizontalLayout(firstNameField, lastNameField);
-        div.setMinWidth(100, Unit.PERCENTAGE);
+        HorizontalLayout nameLayout = new HorizontalLayout(firstNameField, lastNameField);
+        nameLayout.setWidthFull();
+        nameLayout.setJustifyContentMode(JustifyContentMode.BETWEEN);
 
         emailField = new TextField("Email");
         emailField.setMaxWidth(100, Unit.PERCENTAGE);
@@ -61,24 +61,26 @@ public class MainView extends VerticalLayout{
 
         acceptedPrivacyPolicyBox = new Checkbox("Hiermiet bestätige ich, dass ich die Datenschutzerklärung gelesen habe und akzeptiere.");
 
-        Button acceptButton = new Button("Absenden");
+        acceptButton = new Button("Absenden");
         acceptButton.addClickListener(event -> addCustomer());
+        acceptButton.setEnabled(false);
         HorizontalLayout acceptButtonLayout = new HorizontalLayout(acceptButton);
         acceptButtonLayout.setWidthFull();
         acceptButtonLayout.setAlignItems(Alignment.CENTER);
         acceptButtonLayout.setJustifyContentMode(JustifyContentMode.CENTER);
 
-        VerticalLayout form = new VerticalLayout(image, div,  emailField, acceptedPrivacyPolicyBox, acceptButtonLayout);
+        VerticalLayout form = new VerticalLayout(image, nameLayout,  emailField, acceptedPrivacyPolicyBox, acceptButtonLayout);
         form.getStyle().set("background-color", "white");
-        form.setHeightFull();
+        form.getStyle().set("margin-top", "5%");
+        form.setHeight(60, Unit.PERCENTAGE);
         form.setWidth(22, Unit.PERCENTAGE);
+        form.getStyle().set("filter", "drop-shadow(0px 0px 120px #000)");
         add(form);
 
+        setupChangeListener();
     }
 
     public void addCustomer() {
-
-
         val email = emailField.getValue();
         val firstName = firstNameField.getValue();
         val lastName = lastNameField.getValue();
@@ -98,5 +100,28 @@ public class MainView extends VerticalLayout{
 
         System.out.println(customer);
 
+    }
+
+    public void check() {
+        val email = emailField.getValue();
+        val firstName = firstNameField.getValue();
+        val lastName = lastNameField.getValue();
+
+        if(email == null || firstName == null|| lastName == null) {
+            acceptButton.setEnabled(false);
+            return;
+        }
+        if(!(email.isBlank() || firstName.isBlank() || lastName.isBlank()) && acceptedPrivacyPolicyBox.getValue()) {
+            acceptButton.setEnabled(true);
+            return;
+        }
+        acceptButton.setEnabled(false);
+    }
+
+    public void setupChangeListener() {
+        firstNameField.addValueChangeListener(value -> check());
+        lastNameField.addValueChangeListener(value -> check());
+        emailField.addValueChangeListener(value -> check());
+        acceptedPrivacyPolicyBox.addValueChangeListener(value -> check());
     }
 }
